7584064f9d0e888c805a805a9ec0f9560cece182
ad-victoriam-101 <keeper24@gmail.com>
2018-09-23 12:55:02 -0700

    cards == started/routed != correct

--- public/app.js
+++ public/app.js
@@ -4,8 +4,14 @@ function displayResults(WebScrapeData) {
         var card = $('<card>').append(
             $("<card-title>").text(data.title),
             $("<card-body").text(data.link),
-            
-
+            $("<button>").text("Save This link").addClass("btn btn-primary"),
+            $(".btn").attr("id","isSaved")
         );
+        card.addClass("col-md-3");
+        $("#mainContent").append(card);
     });
 }
+
+$.getJSON("/all", data =>{
+    displayResults(data);
+});
