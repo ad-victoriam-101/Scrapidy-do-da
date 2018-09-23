dfe403552c5ec057fcfe5b43c8dc56a080a77b88
ad-victoriam-101 <keeper24@gmail.com>
2018-09-22 21:42:30 -0700

    fixed issues with routes

--- server.js
+++ server.js
@@ -34,5 +34,60 @@ db.on("error", (error) => {
 });
 
 app.get("/", (req, res) => {
+    if(!req){
     res.send(index.html);
+    }
+    res.send("Hello World");
 });
+// Retrieve scrapped data from the DB
+app.get("all", (req, res) =>{
+    db.WebScrapeData.find({}, function(error, found){
+        if (error){
+            console.log(error);
+        }
+        else{
+            res.json(found);
+        }
+    });
+});
+
+app.get("/scrape", function(req, res) {
+    // Make a request for the news section of `ycombinator`
+    request("https://news.ycombinator.com/", function(error, response, html) {
+      // Load the html body from request into cheerio
+      var $ = cheerio.load(html);
+      // For each element with a "title" class
+      $(".title").each(function(i, element) {
+        // Save the text and href of each link enclosed in the current element
+        var title = $(element).children("a").text();
+        var link = $(element).children("a").attr("href");
+  
+        // If this found element had both a title and a link
+        if (title && link) {
+          // Insert the data in the scrapedData db
+          db.scrapedData.insert({
+            title: title,
+            link: link
+          },
+          function(err, inserted) {
+            if (err) {
+              // Log the error if one is encountered during the query
+              console.log(err);
+            }
+            else {
+              // Otherwise, log the inserted data
+              console.log(inserted);
+            }
+          });
+        }
+      });
+    });
+  
+    // Send a "Scrape Complete" message to the browser
+    res.send("Scrape Complete");
+  });
+
+
+  app.listen(3000, function() {
+    console.log("App running on port 3000!");
+  });
